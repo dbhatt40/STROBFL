e3598d1aaf138d4ae7a5be8de612e6b6600230c9
dbhatt40 <62491266+dbhatt40@users.noreply.github.com>
2025-10-20 16:03:52 -0500

    Update DataCollection.sh

--- DataCollection.sh
+++ DataCollection.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python STROBFLMain.py --dataset=census --E=1 --T=2 --train --model_num=0 --optimizer=strsgd --ls=10 --k=2 --C=0.5 --iid=0
+python STROBFLMain.py --dataset=uci-sensor --E=1 --T=2 --train --model_num=0 --optimizer=sgd --ls=10 --k=2 --C=0.5 --iid=0
 
